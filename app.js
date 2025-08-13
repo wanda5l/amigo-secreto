856ddbcb292d3cb4d9b5123496dcb603c38eeade
Wanda <wandaleiva05@gmail.com>
2025-08-13 00:04:43 -0300

    agregando funciones principales

--- app.js
+++ app.js
@@ -1,1 +1,30 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos= [];
+
+function agregarAmigo(){
+    let amigo = document.querySelector('#amigo');
+    if(amigo.value === ""){
+        alert('Por favor, inserte un nombre.');
+    } else {
+        amigos.push(amigo.value);
+    }
+    console.log(amigos); //borrar luego
+
+    limpiarCaja();
+    lista();
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value="";
+}
+
+function lista() {
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ""; 
+
+    for (let i = 0; i < amigos.length; i++) {
+        let li = document.createElement('li'); 
+        li.textContent = amigos[i];            
+        listaAmigos.appendChild(li);           
+    }
+}
